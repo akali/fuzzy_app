916937cc08815e611689ccfee644ad504e2a9584
Aisultan Kali <aisultan.kali@gmail.com>
2020-03-30 23:54:00 +0600

    fix fuzyy typo

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn fuzzy_app.wsgi.application --log-file -
+web: gunicorn fuzzy_app.wsgi --log-file -
--- fuzyy_app/urls.py
+++ fuzyy_app/urls.py
@@ -1,4 +1,4 @@
-"""fuzyy_app URL Configuration
+"""fuzzy_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
